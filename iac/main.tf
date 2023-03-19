9c4df5c49f5376e143fc7bcd6c413b223cf0793a
Joao Barbosa <joao.barbosa@ilegra.com>
2023-03-19 13:21:54 -0300

    add workflows iac

--- iac/main.tf
+++ iac/main.tf
@@ -10,9 +10,4 @@ terraform {
   }
 }
 
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-  secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY_DEV }}"
-
-}
+provider "aws" {}
