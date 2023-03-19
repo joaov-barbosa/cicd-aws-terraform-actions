20bcc964df71a12fea7570c04271fe5d7e8876c7
Joao Barbosa <joao.barbosa@ilegra.com>
2023-03-19 13:42:58 -0300

    add data policy

--- iac/iam.tf
+++ iac/iam.tf
@@ -22,12 +22,12 @@ resource "aws_iam_role" "test_role" {
 resource "aws_iam_policy_attachment" "demo-attach" {
   name       = "demo-attachment"
   roles      = [aws_iam_role.test_role.name]
-  policy_arn = aws_iam_policy.demo-s3-policy.arn
+  policy_arn = data.aws_iam_policy.poly-code-deploy-ec2.arn
 }
 resource "aws_iam_instance_profile" "demo-profile" {
   name = "demo_profile"
   role = aws_iam_role.demo-role.name
 }
-data "aws_iam_policy" "example" {
-  arn = "AWSCodeDeployRole"
+data "aws_iam_policy" "policy-code-deploy-ec2" {
+  arn = "AmazonEC2RoleForAWSCodeDeploy"
 }
