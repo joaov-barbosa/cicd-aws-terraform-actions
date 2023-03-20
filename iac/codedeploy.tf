2bedda3dfc20cf016de22782d0c376a4a8cbe47b
Joao Barbosa <joao.barbosa@ilegra.com>
2023-03-20 09:43:55 -0300

    arn

--- iac/codedeploy.tf
+++ iac/codedeploy.tf
@@ -4,8 +4,7 @@ resource "aws_codedeploy_app" "example" {
 resource "aws_codedeploy_deployment_group" "example" {
   app_name              = aws_codedeploy_app.example.name
   deployment_group_name = "example-group"
-  service_role_arn      = aws_iam_role.codedeploy_service.policy_arn
-
+  service_role_arn      = aws_iam_role.codedeploy_service.arn
     ec2_tag_set {
       ec2_tag_filter {
       Name   = "http_server"
