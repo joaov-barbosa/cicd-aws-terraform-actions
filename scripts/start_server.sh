159c934f8f1a68b301809ab8119bbd14a9ec6cee
Joao Barbosa <joao.barbosa@ilegra.com>
2023-03-20 12:07:58 -0300

    changed

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-cd /home/ubuntu/cicd-aws-terraform-actions 
-nohup python3 Main.py & 
+serverfile="/lib/systemd/system/mypythonservice.service"
+echo "[Unit]" > $serverfile
+echo "Description=My Python Service" > $serverfile
+echo "After=multi-user.target" >> $serverfile
+
+echo "[Service]" >> $serverfile
+echo "Type=idle" >> $serverfile
+echo "ExecStart=/usr/bin/python3.8 /home/ubuntu/Main.py" >> $serverfile
+echo "Restart=on-failure" >> $serverfile
+
+echo "[Install]" >> $serverfile
+echo "WantedBy=multi-user.target" >> $serverfile
+cat $serverfile
+
+sudo chmod 644 /lib/systemd/system/mypythonservice.service
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable mypythonservice.service
