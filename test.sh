2874daf63a383cc47c32ae38cb886d93560a1b7d
Joao Barbosa <joao.barbosa@ilegra.com>
2023-03-24 16:19:25 -0300

    test20

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+aws configure set aws_access_key_id ${{ secrets.AWS_ACCESS_KEY_ID }} --profile default
+aws configure set aws_secret_access_key ${{ secrets.AWS_SECRET_ACCESS_KEY }} --profile default
+aws configure set region ${{ vars.AWS_DEFAULT_REGION }} --profile default
