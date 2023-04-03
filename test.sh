d67dfcd263e4889b31f6a683e805fb1793631b09
Joao Barbosa <joao.barbosa@ilegra.com>
2023-04-03 11:25:13 -0300

    remove

--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-aws configure set aws_access_key_id ${{ secrets.AWS_ACCESS_KEY_ID }} --profile default
-aws configure set aws_secret_access_key ${{ secrets.AWS_SECRET_ACCESS_KEY }} --profile default
-aws configure set region ${{ vars.AWS_REGION }} --profile default
